f8caff86d4f4935be62bf33c21c43b67fa889229
Trey Hampton <dan.rossiters@gmail.com>
2013-08-08 19:58:41 -0500

    pulled from remote to local
    sftp to site so as not to commmit unwanted changes to remote, and got
    the most recent known correct file

--- web/test_item.php
+++ web/test_item.php
@@ -1,19 +1,52 @@
-<? php
+<?php
 require "rf/razorflow.php";
+
+$connection = mysqli_connect("devdb.fulgentcorp.com","2013teamb","29G8l!06J82ofpPw","2013teamb");
+$token = $_GET['test_item'];
+
+
+$project = mysqli_query($connection, "SELECT * FROM "."Project WHERE ProjectName = '$token'");
+$projectInfo = mysqli_fetch_array($project);
+$ProjectID = $projectInfo['ProjectID'];
+$ProjectName = $projectInfo['ProjectName'];
+$DateCreated = $projectInfo['DateCreated'];
+$DateCompleted = $projectInfo['DateCompleted'];
+$Creator = $projectInfo['Creator'];
+$CurrentSprint = $projectInfo['CurrentSprint'];
+$SprintDuration = $projectInfo['SprintDuration'];
+
 
 
 $dataSource = new MySQLDataSource('2013teamb', '2013teamb', '29G8l!06J82ofpPw', 'devdb.fulgentcorp.com');
+$dataSource->setSQLSource("Sprint");
+$sprintRange = new ChartComponent();
+$sprintRange->setCaption ("Sprint Metrics");
+$sprintRange->setDataSource($dataSource);
+$sprintRange->addCondition("Sprint.ProjectID", "==", "$ProjectID");
+//$sprintRange->setLabelExpression("Sprint", "Sprint.SprintNum");
+$sprintRange->setLabelExpression("Day", "Sprint.FinishTime", array(
+														'timestampRange' => 'time',
+														'timeUnit' => 'day',
+														'customTimeUnitPath' => array('year', 'month', 'day'),
+														'autoDrill' => true
+));
 
-$dataSource->setSQLSource('Item');
+//$sprintRange->setYAxis("Day");
+$sprintRange->addSeries("Day", "SprintNum");
+//$sprintRange->setOption('limit', 10);
+Dashboard::addComponent($sprintRange);
 
-// Create a chart to show aggregated sales by genre
-$itemPriority = new ChartComponent();
-$itemPriority->setCaption ("Item Priority");
-$itemPriority->setDataSource($dataSource);
-$itemPriority->setLabelExpression("Name", "Item.ItemName");
-$itemPriority->addSeries("Priority", "Item.Priority");
-$itemPriority->setOption('limit', 10);
-Dashboard::addComponent($itemPriority);
+
+$itemSource = new MySQLDataSource('2013teamb', '2013teamb', '29G8l!06J82ofpPw', 'devdb.fulgentcorp.com');
+$itemSource->setSQLSource("Item");
+$itemRange = new ChartComponent();
+$itemRange->setCaption ("Item Metrics");
+$itemRange->setDataSource($itemSource);
+$itemRange->addCondition("Item.ProjectID", "==", "$ProjectID");
+$itemRange->setLabelExpression("Name", "Item.ItemName");
+$itemRange->addSeries("Item", "Item.Priority");
+Dashboard::addComponent($itemRange);
+
 
 
 Dashboard::Render();
